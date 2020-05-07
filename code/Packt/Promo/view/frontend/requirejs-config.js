7129408a07b388bc8092cc6b12268f61f1339e8d
binhct201097 <caobinh.20101997@gmail.com>
2020-05-07 20:43:27 +0700

    add slide Owl Carousel

---
+++ code/Packt/Promo/view/frontend/requirejs-config.js
@@ -0,0 +1,14 @@
+var config = {
+    "map":{
+    "*":{
+        promo: "Packt_Promo/js/promo",
+        hello: "Packt_Promo/js/hello"
+    }
+    },
+    "shim": {
+    "Packt_Promo/js/jquery.jcarousel-core.min": ["jquery"],
+    "Packt_Promo/js/jquery.jcarousel-control.min": ["jquery"],
+    "Packt_Promo/js/jquery.jcarousel-pagination.min": ["jquery"]
+    }
+   };
+   
